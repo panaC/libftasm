88a24613f9424db0f564e792434ebfa95128b18f
p.leroux <pleroux@student.42.fr>
2019-11-28 18:32:07 +0100

    upgrade cat, bug on stdin

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -6,7 +6,7 @@
 #    By: pleroux <pleroux@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/11/11 21:52:25 by pleroux           #+#    #+#              #
-#    Updated: 2019/11/16 15:10:22 by pleroux          ###   ########.fr        #
+#    Updated: 2019/11/28 18:16:46 by pleroux          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,22 +21,24 @@ segment .bss ; unitialized data
 %define WRITE			4
 %define READ			3
 %define SIZE			1000
+%define STACK			SIZE
 
 segment .text
 		global _ft_cat
-		extern _malloc
+;		extern _malloc
 
 _ft_cat:
-		enter			8, 0			; save rsp to pick up 2 bytes
-		push			rdi				; save in stack fd
+		enter			STACK, 0		; save rsp to pick up 2 bytes
+		mov				r8, rdi			; save in stack fd
+		mov				r9, rbp
 
-		mov				rdi, SIZE		; malloc 1000 bytes
-		call			_malloc
-		test			rax, rax
-		je				return
-
-		pop				r8				; save data before loop, r8 = fd
-		mov				r9, rax			; save malloc ptr in r9
+;		mov				rdi, SIZE		; malloc 1000 bytes
+;		call			_malloc
+;		test			rax, rax
+;		je				return
+;
+;		pop				r8				; save data before loop, r8 = fd
+;		mov				r9, rax			; save malloc ptr in r9
 
 read:
 		mov				rdi, r8			; read SIZE bytes to r9
